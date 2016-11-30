dfb4331b7e6b25431705903b009497c0725f233f
Piwi <piwi82@free.fr>
2016-11-30 17:45:00 +0100

    Updated constant HOST to "www.cpasbien.cm"

--- Download Station/BTsearch/CpasBien/cpasbien.php
+++ Download Station/BTsearch/CpasBien/cpasbien.php
@@ -39,7 +39,8 @@ private static $plugin;
 private static $count = 0;
 const PAGE_MAX = 10;
 const PROTOCOL = "http";
-const HOST = "www.cpasbien.io";	// 104.28.8.24, 104.28.9.24
+//const HOST = "www.cpasbien.io";	// 104.28.8.24, 104.28.9.24
+const HOST = "www.cpasbien.cm";	// 2400:cb00:2048:1::6818:7626, 2400:cb00:2048:1::6818:7626, 104.24.118.38, 104.24.119.38
 const URL = "%s://%s/recherche/%s/page-%s";
 const USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
 const REG_RESULT = '#<a\s+href="(?<page>[^"]+)"\s+title="(?<category>[^<]+)<br(\s*/)?>[^<]+(?<datetime>(?:[0-9]{2}[^0-9]){2}[0-9]{4})"(?:\s+[a-z]+="[^"]*")*>(?<title>[^<]+)</a><div(?:\s+[a-z]+="[^"]*")*>(?<size>[^<]+)(?:\&nbsp;)+?</div><div(?:\s+[a-z]+="[^"]*")*><span(?:\s+[a-z]+="[^"]*")*>(?<seeds>[0-9]+)</span></div><div(?:\s+[a-z]+="[^"]*")*>(?<leechs>[0-9]+)</div>#i';
